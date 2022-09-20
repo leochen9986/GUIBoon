2dccbd47f601cb6cad45461f26e872298a619bd2
leochen9986 <92980109+leochen9986@users.noreply.github.com>
2022-09-20 14:29:36 +0800

    Update test_queue_noprocess.py

--- test_queue_noprocess.py
+++ test_queue_noprocess.py
@@ -17,7 +17,7 @@ else:
     
 
 class FileVideoStream:
-	def __init__(self, path, queueSize=256):
+	def __init__(self, path, queueSize=128):
 		# initialize the file video stream along with the boolean
 		# used to indicate if the thread should be stopped or not
 		self.stream = cv2.VideoCapture(path)
