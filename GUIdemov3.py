b6be8898b66af67bed7f0d684293634e1018e2f6
winterkim360 <84696298+winterkim360@users.noreply.github.com>
2022-10-08 11:18:40 +0800

    Update GUIdemov3.py

--- GUIdemov3.py
+++ GUIdemov3.py
@@ -23,7 +23,7 @@ def get_json_data():
     '''
     data={"Motion_detected":0,"Bottle_placed":0,"wifi_status":1,"filling_status":0,"cloud_status":1,
           "total_water_dispensed":10200,"tds_outlet":100,"tds_inlet":200,"operational_minutes":122,
-          "ph":7.2,"tank_level":1,"trip_state":0}
+          "ph":7.2,"tank_level":1,"trip_state":0,"error_code":0}
     
     return data
 
@@ -36,13 +36,13 @@ class GUI:
         self.esp32_data=get_json_data()        
         
         self.window = window
-        #self.window.attributes('-fullscreen', True)
+        self.window.attributes('-fullscreen', True)
         self.window.bind("<Escape>", lambda x:  self.window.destroy())
         self.window.geometry("768x1366")
         self.window.title(window_title)
         
         self.screen_width = self.window.winfo_screenheight()
-        self.screen_height = int((self.screen_width/16)*8.5)
+        self.screen_height = int((self.screen_width/16)*8.25)
         
         # variable of event and screen counter
         self.event_counter_n = 1
@@ -50,7 +50,7 @@ class GUI:
         self.screen_change_event=False
         
         self.canvas_icon = tk.Canvas(window, width = self.screen_height,
-                         height = 50,background='white')
+                         height = 20,background='white')
         
         self.canvas_icon.pack()       
         
@@ -98,34 +98,56 @@ class GUI:
             #volume and tds label
         self.volume_var = tk.StringVar()
         self.volume_var.set("Volume:0")
-        self.volume_label = tk.Label(self.window, textvariable=self.volume_var, width=15 ,font=("Arial", 15),bg="white")
+        self.volume_label = tk.Label(self.window, textvariable=self.volume_var, width=12 ,font=("Arial", 12),bg="white")
         self.volume_label.update_idletasks()
         self.volume_label.place(x=int(self.screen_height*0.35),y=int(self.screen_width*0.73))
         self.volume_label.place_forget()
         
         
         self.tds_var = tk.StringVar()
-        self.tds_var.set("TDS:0")
-        self.tds_label = tk.Label(self.window, textvariable=self.tds_var, width=15 ,font=("Arial", 15),bg="white")
+        self.tds_var.set("TDS:0~0")
+        self.tds_label = tk.Label(self.window, textvariable=self.tds_var, width=12 ,font=("Arial", 12),bg="white")
         self.tds_label.update_idletasks()
         self.tds_label.place(x=int(self.screen_height*0.35)+160,y=int(self.screen_width*0.73))
         self.tds_label.place_forget()        
-        
+ 
+        self.ph_var = tk.StringVar()
+        self.ph_var.set("pH:0")
+        self.ph_label = tk.Label(self.window, textvariable=self.ph_var, width=12 ,font=("Arial", 12),bg="white")
+        self.ph_label.update_idletasks()
+        self.ph_label.place(x=int(self.screen_height*0.35)+160,y=int(self.screen_width*0.73))
+        self.ph_label.place_forget()  
+ 
         ###############production layer#############################        
         
 
         ############icon layers#####################
          #wifi
         self.current_wifi="media/icons/wifi.png"
-        self.wifiIMG=Image.open(self.current_wifi).resize((50, 50))
+        self.wifiIMG=Image.open(self.current_wifi).resize((20, 20))
         self.wifiImage = ImageTk.PhotoImage(self.wifiIMG)
-        self.wifiImage_container = self.canvas_icon.create_image(500, 25, image=self.wifiImage)        
+        self.wifiImage_container = self.canvas_icon.create_image(540, 10, image=self.wifiImage)        
 
          #cloud
         self.current_cloud="media/icons/cloud.png"
-        self.cloudIMG=Image.open(self.current_cloud).resize((50, 50))
+        self.cloudIMG=Image.open(self.current_cloud).resize((20, 20))
         self.cloudImage = ImageTk.PhotoImage(self.cloudIMG)
-        self.cloudImage_container = self.canvas_icon.create_image(450, 25, image=self.cloudImage)  
+        self.cloudImage_container = self.canvas_icon.create_image(510, 10, image=self.cloudImage)
+        
+        #alert
+        self.alert_var = tk.StringVar()
+        self.alert_var.set("0")
+        self.alert_label = tk.Label(self.canvas_icon, textvariable=self.alert_var, width=5 ,font=("Arial", 10),bg="white")
+        self.alert_label.update_idletasks()
+        self.alert_label.place(x=455,y=1)               
+
+
+        #treated water
+        self.treatedvolume_var = tk.StringVar()
+        self.treatedvolume_var.set("Treated Water: 0")
+        self.treatedvolume_label = tk.Label(self.canvas_icon, textvariable=self.treatedvolume_var, width=20 ,font=("Arial", 10),bg="white")
+        self.treatedvolume_label.update_idletasks()
+        self.treatedvolume_label.place(x=10,y=1)    
         ############icon layers#####################
         
         
@@ -178,8 +200,13 @@ class GUI:
             self.wifiImage = ImageTk.PhotoImage(self.cloudIMG)
             self.canvas.itemconfig( self.cloudImage_container,image =self.cloudImage)  
 
-
-
+        #update alert icon
+        self.alert_var.set(str(self.esp32_data["error_code"]))
+        self.alert_label.update_idletasks()
+        
+        #update treated volume
+        self.treatedvolume_var.set("Treated Water: "+str(self.esp32_data["total_water_dispensed"]))
+        self.treatedvolume_label.update_idletasks()
         
         #print(self.testproximitybool)
         #Screen1
@@ -199,6 +226,7 @@ class GUI:
                         self.player._set_property("pause",False)
                         self.volume_label.place_forget()
                         self.tds_label.place_forget()
+                        self.ph_label.place_forget()
                         self.screen_change_event=True
                 
                 
@@ -241,11 +269,15 @@ class GUI:
             self.volume_var.set("Volume:"+str(self.esp32_data["total_water_dispensed"]))
             self.volume_label.update_idletasks()
 
-            self.tds_var.set("TDS:"+str(self.esp32_data["tds_outlet"]))
+            self.tds_var.set("TDS:"+str(self.esp32_data["tds_inlet"])+"~"+str(self.esp32_data["tds_outlet"]))
             self.tds_label.update_idletasks()
 
+            self.ph_var.set("ph:"+str(self.esp32_data["ph"]))
+            self.ph_label.update_idletasks()
+
             self.volume_label.place(x=int(self.screen_height*0.35),y=int(self.screen_width*0.73))
-            self.tds_label.place(x=int(self.screen_height*0.35)+200,y=int(self.screen_width*0.73))
+            self.tds_label.place(x=int(self.screen_height*0.35)+130,y=int(self.screen_width*0.73))
+            self.ph_label.place(x=int(self.screen_height*0.35)+250,y=int(self.screen_width*0.73))
             
             if self.player._get_property("pause") or self.state=="proximity":
                 self.screen_counter_n=2
