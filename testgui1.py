0ed196f8742f31c1569a36b35889d06951b094bf
leochen9986 <92980109+leochen9986@users.noreply.github.com>
2022-10-19 13:25:10 +0800

    UP2

--- testgui1.py
+++ testgui1.py
@@ -3,6 +3,8 @@ import mpv
   
 root=tk.Tk()
 root.geometry("768x1366")
+root.attributes('-fullscreen', True)
+root.bind("<Escape>", lambda x:  root.destroy())
 player = mpv.MPV(wid=str(int(root.winfo_id())), input_default_bindings=True,input_vo_keyboard=True,osc=True)
 player.play("media/marketing_layer/event1/1.mp4")
   
