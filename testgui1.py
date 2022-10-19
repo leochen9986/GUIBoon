a5f65ac5a0608144efd5a4342404aabd3f43f11c
leochen9986 <92980109+leochen9986@users.noreply.github.com>
2022-10-19 10:38:30 +0800

    up

--- testgui1.py
+++ testgui1.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import mpv
   
 root=tk.Tk()
-root.geometry("500x280")
+root.geometry("768x1366")
 player = mpv.MPV(wid=str(int(root.winfo_id())), input_default_bindings=True,input_vo_keyboard=True,osc=True)
 player.play("media/marketing_layer/event1/1.mp4")
   
---
+++ testgui2.py
@@ -0,0 +1,58 @@
+import tkinter as tk
+import mpv
+import time
+from PIL import Image, ImageTk
+import serial
+import json
+
+class GUI:
+    def __init__(self, window, window_title, video_source="media/marketing_layer/event1/1.mp4"):
+        
+        
+        self.refresh_time=300
+        
+        self.esp32_data_current=""
+        
+        if self.esp32_data_current=="":
+            self.esp32_data={"Motion_detected":0,"Bottle_placed":0,"wifi_status":1,"filling_status":0,"cloud_status":1,
+                  "total_water_dispensed":10200,"tds_outlet":100,"tds_inlet":200,"operational_minutes":122,
+                  "ph":7.2,"tank_level":1,"trip_state":0,"error_code":0}
+        else:
+            self.esp32_data=self.esp32_data_current
+        
+        self.window = window
+        self.window.attributes('-fullscreen', True)
+        self.window.bind("<Escape>", lambda x:  self.window.destroy())
+        self.window.geometry("768x1366")
+        self.window.title(window_title)
+        
+        self.screen_width = self.window.winfo_screenheight()
+        self.screen_height = int((self.screen_width/16)*7)
+        
+        # variable of event and screen counter
+        self.event_counter_n = 1
+        self.screen_counter_n = 1    
+        self.screen_change_event=False
+        
+        self.canvas_icon = tk.Canvas(window, width = self.screen_height,
+                         height = 20,background='white')
+        
+        self.canvas_icon.pack(fill=tk.BOTH,expand=1)       
+        
+  
+        self.canvas = tk.Canvas(window, width = self.screen_height,
+                         height = self.screen_width,background='white')
+        
+        self.canvas.pack(fill=tk.BOTH,expand=1)
+        
+
+        
+        self.player = mpv.MPV(wid=str(int(self.canvas.winfo_id())), vo='x11')
+
+        self.player._set_property("keep-open", "always")
+        self.player._set_property("video-aspect","0.633")
+        self.player.play("media/marketing_layer/event{}/{}.mp4".format(self.event_counter_n,self.screen_counter_n))
+        
+        self.window.mainloop()    
+  
+GUI(tk.Tk(), "Tkinter and OpenCV")
